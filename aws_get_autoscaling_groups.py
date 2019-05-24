05f670e752e37d00555fab76f92e52a87c327bec
Nalin Pushpakumara <nalinone77@gmail.com>
2019-05-24 15:47:30 +0530

    add aws_get_autoscaling_groups.py

---
+++ aws_get_autoscaling_groups.py
@@ -0,0 +1,17 @@
+import boto3
+from boto3.session import Session
+
+# Initialize Session
+s = Session()
+ #Get ec2 available regions
+ec2_regions = s.get_available_regions('ec2')
+
+#Loop through region
+for region in ec2_regions:
+    client = boto3.client('autoscaling', region_name=region)
+    response = client.describe_auto_scaling_groups()
+    
+    print("=========================> REGION: %s <=========================")%(region)
+    #Get each autoscaling group info from one region and display
+    for asg in response['AutoScalingGroups']:
+        print(asg['AutoScalingGroupName'])
